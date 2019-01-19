5e52f641555bf98962a2be59aa4ceffaa8b3913c
cuteant <cuteant@outlook.com>
2019-01-19 17:59:52 +0800

    IObjectTypeDeserializer.TryDeserialize

--- src/CuteAnt.Extensions.Serialization.Abstractions/IObjectTypeDeserializer.cs
+++ src/CuteAnt.Extensions.Serialization.Abstractions/IObjectTypeDeserializer.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 
 namespace CuteAnt.Extensions.Serialization
 {
-  public interface IObjectTypeDeserializer
-  {
-    TValue Deserialize<TValue>(IDictionary<string, object> dictionary);
-    TValue Deserialize<TValue>(IDictionary<string, object> dictionary, string key);
-    TValue Deserialize<TValue>(IDictionary<string, object> dictionary, string key, TValue defaultValue);
+    public interface IObjectTypeDeserializer
+    {
+        TValue Deserialize<TValue>(IDictionary<string, object> dictionary);
+        TValue Deserialize<TValue>(IDictionary<string, object> dictionary, string key);
+        TValue Deserialize<TValue>(IDictionary<string, object> dictionary, string key, TValue defaultValue);
+        bool TryDeserialize<TValue>(IDictionary<string, object> dictionary, string key, out TValue value);
 
 
-    TValue Deserialize<TValue>(object value);
-    TValue Deserialize<TValue>(object value, TValue defaultValue);
-    object Deserialize(object value, Type objectType, bool allowNull = false);
-  }
+        TValue Deserialize<TValue>(object value);
+        TValue Deserialize<TValue>(object value, TValue defaultValue);
+        object Deserialize(object value, Type objectType, bool allowNull = false);
+    }
 }
--- src/CuteAnt.Extensions.Serialization/Json/JsonObjectTypeDeserializer.cs
+++ src/CuteAnt.Extensions.Serialization/Json/JsonObjectTypeDeserializer.cs
@@ -8,181 +8,193 @@ using Newtonsoft.Json.Linq;
 
 namespace CuteAnt.Extensions.Serialization
 {
-  /// <summary>Support deserialization of 'objects' from messages into actual types. Objects should have been
-  /// serialized with JSON.NET (or some similar serializer).</summary>
-  public sealed class JsonObjectTypeDeserializer : IObjectTypeDeserializer
-  {
-    /// <summary>Instance</summary>
-    public static readonly IObjectTypeDeserializer Instance;
-
-    private readonly ObjectPool<JsonSerializer> _jsonSerializerPool;
-
-    #region @@ Constructors @@
-
-    static JsonObjectTypeDeserializer()
+    /// <summary>Support deserialization of 'objects' from messages into actual types. Objects should have been
+    /// serialized with JSON.NET (or some similar serializer).</summary>
+    public sealed class JsonObjectTypeDeserializer : IObjectTypeDeserializer
     {
-      Instance = new JsonObjectTypeDeserializer();
-    }
+        /// <summary>Instance</summary>
+        public static readonly IObjectTypeDeserializer Instance;
 
-    public JsonObjectTypeDeserializer()
-      : this(JsonConvertX.DefaultDeserializerSettings)
-    {
-    }
+        private readonly ObjectPool<JsonSerializer> _jsonSerializerPool;
 
-    public JsonObjectTypeDeserializer(JsonSerializerSettings jsonSettings)
-    {
-      if (null == jsonSettings) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.jsonSettings); }
+        #region @@ Constructors @@
 
-      _jsonSerializerPool = JsonConvertX.GetJsonSerializerPool(jsonSettings);
-    }
-
-    #endregion
-
-    TValue IObjectTypeDeserializer.Deserialize<TValue>(IDictionary<string, object> dictionary)
-    {
-      if (null == dictionary) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.dictionary); }
-
-      var objDictionary = JObject.FromObject(dictionary ?? new Dictionary<string, object>());
-
-      using (var jsonReader = new JTokenReader(objDictionary))
-      {
-        var jsonDeserializer = _jsonSerializerPool.Take();
-
-        try
+        static JsonObjectTypeDeserializer()
         {
-          return jsonDeserializer.Deserialize<TValue>(jsonReader);
+            Instance = new JsonObjectTypeDeserializer();
         }
-        finally
+
+        public JsonObjectTypeDeserializer()
+          : this(JsonConvertX.DefaultDeserializerSettings)
         {
-          _jsonSerializerPool.Return(jsonDeserializer);
         }
-      }
-    }
 
-    TValue IObjectTypeDeserializer.Deserialize<TValue>(IDictionary<string, object> dictionary, string key)
-    {
-      if (null == dictionary) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.dictionary); }
-
-      if (dictionary.TryGetValue(key, out object value) || TryGetValueCamelCase(key, dictionary, out value))
-      {
-        return (TValue)Deserialize(value, typeof(TValue), false);
-      }
-      throw new KeyNotFoundException($"The key was not present: {key}");
-    }
-
-    TValue IObjectTypeDeserializer.Deserialize<TValue>(IDictionary<string, object> dictionary, string key, TValue defaultValue)
-    {
-      if (null == dictionary) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.dictionary); }
-
-      if (dictionary.TryGetValue(key, out object value) || TryGetValueCamelCase(key, dictionary, out value))
-      {
-        return (TValue)Deserialize(value, typeof(TValue), false);
-      }
-      return defaultValue;
-    }
-
-    TValue IObjectTypeDeserializer.Deserialize<TValue>(object value) => (TValue)Deserialize(value, typeof(TValue), false);
-
-    TValue IObjectTypeDeserializer.Deserialize<TValue>(object value, TValue defaultValue)
-    {
-      var result = Deserialize(value, typeof(TValue), true);
-      if (null == result) { return defaultValue; }
-
-      return (TValue)result;
-    }
-
-    public object Deserialize(object value, Type objectType, bool allowNull = false)
-    {
-      if (value?.GetType() == TypeConstants.CombGuidType)
-      {
-        if (objectType == TypeConstants.CombGuidType)
+        public JsonObjectTypeDeserializer(JsonSerializerSettings jsonSettings)
         {
-          return value;
+            if (null == jsonSettings) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.jsonSettings); }
+
+            _jsonSerializerPool = JsonConvertX.GetJsonSerializerPool(jsonSettings);
         }
-        else if (objectType == TypeConstants.StringType)
+
+        #endregion
+
+        TValue IObjectTypeDeserializer.Deserialize<TValue>(IDictionary<string, object> dictionary)
         {
-          return value.ToString();
+            if (null == dictionary) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.dictionary); }
+
+            var objDictionary = JObject.FromObject(dictionary ?? new Dictionary<string, object>());
+
+            using (var jsonReader = new JTokenReader(objDictionary))
+            {
+                var jsonDeserializer = _jsonSerializerPool.Take();
+
+                try
+                {
+                    return jsonDeserializer.Deserialize<TValue>(jsonReader);
+                }
+                finally
+                {
+                    _jsonSerializerPool.Return(jsonDeserializer);
+                }
+            }
         }
-        else if (objectType == TypeConstants.GuidType)
+
+        TValue IObjectTypeDeserializer.Deserialize<TValue>(IDictionary<string, object> dictionary, string key)
         {
-          return (Guid)((CombGuid)value);
+            if (null == dictionary) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.dictionary); }
+
+            if (dictionary.TryGetValue(key, out object value) || TryGetValueCamelCase(key, dictionary, out value))
+            {
+                return (TValue)Deserialize(value, typeof(TValue), false);
+            }
+            throw new KeyNotFoundException($"The key was not present: {key}");
         }
-        else if (objectType == TypeConstants.ByteArrayType)
+
+        TValue IObjectTypeDeserializer.Deserialize<TValue>(IDictionary<string, object> dictionary, string key, TValue defaultValue)
         {
-          //return ((CombGuid)value).ToByteArray();
-          return ((CombGuid)value).GetByteArray();
+            if (null == dictionary) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.dictionary); }
+
+            if (dictionary.TryGetValue(key, out object value) || TryGetValueCamelCase(key, dictionary, out value))
+            {
+                return (TValue)Deserialize(value, typeof(TValue), false);
+            }
+            return defaultValue;
         }
-        else if (objectType == typeof(DateTime))
+
+        bool IObjectTypeDeserializer.TryDeserialize<TValue>(IDictionary<string, object> dictionary, string key, out TValue value)
         {
-          return ((CombGuid)value).DateTime;
+            if (null == dictionary) { ThrowHelper.ThrowArgumentNullException(ExceptionArgument.dictionary); }
+
+            if (dictionary.TryGetValue(key, out object rawValue) || TryGetValueCamelCase(key, dictionary, out rawValue))
+            {
+                value = (TValue)Deserialize(rawValue, typeof(TValue), false);
+                return true;
+            }
+            value = default; return false;
         }
-        else if (objectType == typeof(DateTimeOffset))
+
+        TValue IObjectTypeDeserializer.Deserialize<TValue>(object value) => (TValue)Deserialize(value, typeof(TValue), false);
+
+        TValue IObjectTypeDeserializer.Deserialize<TValue>(object value, TValue defaultValue)
         {
-          return new DateTimeOffset(((CombGuid)value).DateTime);
+            var result = Deserialize(value, typeof(TValue), true);
+            if (null == result) { return defaultValue; }
+
+            return (TValue)result;
         }
-        value = value.ToString();
-      }
-      var token = value as JToken ?? new JValue(value);
-      if (token.Type == JTokenType.Null && allowNull) { return null; }
 
-      using (var jsonReader = new JTokenReader(token))
-      {
-        var jsonDeserializer = _jsonSerializerPool.Take();
-
-        try
+        public object Deserialize(object value, Type objectType, bool allowNull = false)
         {
-          return jsonDeserializer.Deserialize(jsonReader, objectType);
+            if (value?.GetType() == TypeConstants.CombGuidType)
+            {
+                if (objectType == TypeConstants.CombGuidType)
+                {
+                    return value;
+                }
+                else if (objectType == TypeConstants.StringType)
+                {
+                    return value.ToString();
+                }
+                else if (objectType == TypeConstants.GuidType)
+                {
+                    return (Guid)((CombGuid)value);
+                }
+                else if (objectType == TypeConstants.ByteArrayType)
+                {
+                    //return ((CombGuid)value).ToByteArray();
+                    return ((CombGuid)value).GetByteArray();
+                }
+                else if (objectType == typeof(DateTime))
+                {
+                    return ((CombGuid)value).DateTime;
+                }
+                else if (objectType == typeof(DateTimeOffset))
+                {
+                    return new DateTimeOffset(((CombGuid)value).DateTime);
+                }
+                value = value.ToString();
+            }
+            var token = value as JToken ?? new JValue(value);
+            if (token.Type == JTokenType.Null && allowNull) { return null; }
+
+            using (var jsonReader = new JTokenReader(token))
+            {
+                var jsonDeserializer = _jsonSerializerPool.Take();
+
+                try
+                {
+                    return jsonDeserializer.Deserialize(jsonReader, objectType);
+                }
+                finally
+                {
+                    _jsonSerializerPool.Return(jsonDeserializer);
+                }
+            }
         }
-        finally
+
+        #region -- 静态方法 --
+
+        public static TValue Deserialize<TValue>(IDictionary<string, object> dictionary)
         {
-          _jsonSerializerPool.Return(jsonDeserializer);
+            return Instance.Deserialize<TValue>(dictionary);
         }
-      }
+
+        public static TValue Deserialize<TValue>(IDictionary<string, object> dictionary, string key)
+        {
+            return Instance.Deserialize<TValue>(dictionary, key);
+        }
+
+        public static TValue Deserialize<TValue>(IDictionary<string, object> dictionary, string key, TValue defaultValue)
+        {
+            return Instance.Deserialize(dictionary, key, defaultValue);
+        }
+
+        public static TValue Deserialize<TValue>(object value)
+        {
+            return Instance.Deserialize<TValue>(value);
+        }
+
+        public static TValue Deserialize<TValue>(object value, TValue defaultValue)
+        {
+            return Instance.Deserialize<TValue>(value, defaultValue);
+        }
+
+        #endregion
+
+        #region **& TryGetValueCamelCase &**
+
+        private static bool TryGetValueCamelCase(string key, IDictionary<string, object> dictionary, out object value)
+        {
+            if (char.IsUpper(key[0]))
+            {
+                var camelCaseKey = StringUtils.ToCamelCase(key);
+                return dictionary.TryGetValue(camelCaseKey, out value);
+            }
+
+            value = null;
+            return false;
+        }
+
+        #endregion
     }
-
-    #region -- 静态方法 --
-
-    public static TValue Deserialize<TValue>(IDictionary<string, object> dictionary)
-    {
-      return Instance.Deserialize<TValue>(dictionary);
-    }
-
-    public static TValue Deserialize<TValue>(IDictionary<string, object> dictionary, string key)
-    {
-      return Instance.Deserialize<TValue>(dictionary, key);
-    }
-
-    public static TValue Deserialize<TValue>(IDictionary<string, object> dictionary, string key, TValue defaultValue)
-    {
-      return Instance.Deserialize(dictionary, key, defaultValue);
-    }
-
-    public static TValue Deserialize<TValue>(object value)
-    {
-      return Instance.Deserialize<TValue>(value);
-    }
-
-    public static TValue Deserialize<TValue>(object value, TValue defaultValue)
-    {
-      return Instance.Deserialize<TValue>(value, defaultValue);
-    }
-
-    #endregion
-
-    #region **& TryGetValueCamelCase &**
-
-    private static bool TryGetValueCamelCase(string key, IDictionary<string, object> dictionary, out object value)
-    {
-      if (char.IsUpper(key[0]))
-      {
-        var camelCaseKey = StringUtils.ToCamelCase(key);
-        return dictionary.TryGetValue(camelCaseKey, out value);
-      }
-
-      value = null;
-      return false;
-    }
-
-    #endregion
-  }
 }
